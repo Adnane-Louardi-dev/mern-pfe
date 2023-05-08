48a7784399db13d7b59532d2ee126c20a5b29206
Adnane Louardi <adnanelouardi.dev@gmail.com>
2023-05-08 20:05:16 +0100

    add ministere route

--- server/index.js
+++ server/index.js
@@ -4,13 +4,15 @@ const express = require('express');
 const app = express();
 const connectToMongo = require('./db');
 const morgan = require("morgan");
+const ministere=require("./routes/partieMinistere")
 
 //connect to cluster
 connectToMongo();
 
 //middlewares
-app.use(cors())
+app.use(cors());
 app.use(morgan("dev"));
+app.use("/ministere",ministere)
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
