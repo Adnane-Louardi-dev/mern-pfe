a3b49846e20b809d7cc7eefeeeac82a68f145bb7
ayaazouggar <132955343+ayaazouggar@users.noreply.github.com>
2023-05-25 14:52:14 +0100

    Create remarquemodels.js

---
+++ server/models/remarquemodels.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const remarqueSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    enum: ['produit', 'rapport'],
+    required: true
+  },
+  produit: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Produit' // Référence au modèle Produit si le type est 'produit'
+  },
+  rapport: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Rapport' // Référence au modèle Rapport si le type est 'rapport'
+  },
+  contenu: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+
+  // Autres champs spécifiques aux remarques
+
+});
+
+module.exports = Remarque;
