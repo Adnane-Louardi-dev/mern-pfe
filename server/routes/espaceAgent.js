3891b7bc856450bbcda8cddfc4023fc108aab195
hatim mordy <hmordy02@gmail.com>
2023-05-27 17:08:27 +0000

    add authentication and autorization to espace agent

--- server/routes/espaceAgent.js
+++ server/routes/espaceAgent.js
@@ -1,38 +1,46 @@
 const express = require('express');
 const router = express.Router();
+const {protect}=require('../middleware/Authmiddleware');
 
+const { getDemandeEnAttend , DsignerDateComm , getDemandeApprouver ,DsignerDateinspection , login , registerUser , inpection } = require('../controllers/agents');
 
-const { getDemandeEnAttend , DsignerDateComm , getDemandeApprouver ,DsignerDateinspection } = require('../controllers/agents');
+// login 
+router.post('/login',login );
+// register inspecteur et instructeur 
+
+ router.post('/Admin/register',protect("Administrateur"),registerUser );
+
+ router.post('/inspection',protect("Inspecteur"),inpection );
 
 
 //consulter les demande en attente  
-router.get('/Admin/Commission',getDemandeEnAttend );
+router.get('/Admin/Commission',protect("Administrateur"),getDemandeEnAttend );
 // designer la date de commission pour une demande 
-router.put('/Admin/Commission/:demandeId/:date',DsignerDateComm);
+router.put('/Admin/Commission/:demandeId/:date',protect("Administrateur"),DsignerDateComm);
 
 // get list instructeure
-//router.get('/Admin/Commission',getListInsructeur);
+//router.get('/Admin/Commission',protect("Administrateur"),getListInsructeur);
 
 // designer le constructeur pour une demande 
-//router.put('/Admin/Commission/:demandeId/:instructeur',Dsignerinstructeur);
+//router.put('/Admin/Commission/:demandeId/:instructeur',protect("Administrateur"),Dsignerinstructeur);
 
 //consulter les demande en attente  approver par instruction 
-router.get('/Admin/Inspection',getDemandeApprouver );
+router.get('/Admin/Inspection',protect("Administrateur"),getDemandeApprouver );
 // designer la date de inspection pour une demande 
-router.put('/Admin/Inspection/:demandeId/:date',DsignerDateinspection);
+router.put('/Admin/Inspection/:demandeId/:date',protect("Administrateur"),DsignerDateinspection);
 
 // get list instructeure
-//router.get('/Admin/inspection',getListInnpecteur);
+//router.get('/Admin/inspection',protect("Administrateur"),getListInnpecteur);
 
 // designer le constructeur pour une demande 
-//router.put('/Admin/Commission/:demandeId/:inspecteur',DesignerInnpecteur);
+//router.put('/Admin/Commission/:demandeId/:inspecteur',protect("Administrateur"),DesignerInnpecteur);
 
 
 
 
 
 // Route pour enregistrer le rapport d'inspection
-//router.put('/inspections/:inspectionId/rapport',enregistrerRapportInspection);
+//router.put('/inspections/:inspectionId/rapport',protect("Inspecteur"),enregistrerRapportInspection);
 
 // Route pour consulter les alertes liées aux inspections
 ///router.get('/alertes', consulterAlertes);
