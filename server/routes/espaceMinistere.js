cca945eb0830470b3a7c67afe80d5924aa91b47d
ayaazouggar <132955343+ayaazouggar@users.noreply.github.com>
2023-06-02 02:47:15 +0100

    Update espaceMinistere.js

--- server/routes/espaceMinistere.js
+++ server/routes/espaceMinistere.js
@@ -6,22 +6,26 @@ const path = require('path');
 const router = express.Router();
 
 
+// Route pour la création du compte ministère de santé
+router.post('/signup', ministereController.createMinistere);
+
+// Route pour l'authentification
+router.post('/login', ministereController.login);
+
 // Route pour la saisie des remarques par produit
 router.post('/saisie-remarque-produit', ministereController.saisieRemarqueProduit);
 
 // Route pour la saisie des remarques sur les rapports
 router.post('/saisie-remarque-rapports', ministereController.saisieRemarqueRapports);
 
-//GET all Demandes
-//router.get('/', getDemandes)
+// Route pour la consultation des rapports 
+router.get('/rapports', ministereController.consulterRapports);
 
-//GET a Demande
-//router.get('/:id', getDemande)
+// Route pour la validation du rapport d'instruction
+router.put('/valider-rapports/:id', ministereController.validerRapports);
 
 // Route pour l'ajout d'un produit avec une image
 router.post('/produits', upload.single('image'), ministereController.ajouterProduit);
 
-
 module.exports = router;
-
 
